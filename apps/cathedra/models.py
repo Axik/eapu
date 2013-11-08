26dbba31ae001b7d630da13b6fef1db6db184cb3
marik <marik912666@mail.ru>
2013-11-08 17:21:57 +0200

    academicx list page, fix models

--- apps/cathedra/models.py
+++ apps/cathedra/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from skd_tools.utils import image_path
 from ckeditor.fields import RichTextField
+from sorl.thumbnail import ImageField
 
 
 class Position(models.Model):
@@ -29,11 +30,39 @@ class Lector(models.Model):
     last_name = models.CharField(_('last name'), max_length=30)
     first_name = models.CharField(_('first name'), max_length=30)
     middle_name = models.CharField(_('middle name'), max_length=30)
-    position_title = models.ForeignKey(Position, verbose_name=_('position'), related_name='position')
-    science_title = models.ForeignKey(Science, verbose_name=_('science'), related_name='science')
+    position_title = models.ForeignKey(Position, verbose_name=_('position'), related_name='l_position')
+    science_title = models.ForeignKey(Science, verbose_name=_('science'), related_name='l_science')
     contact = models.CharField(max_length=64, verbose_name=_('contacts'))
-    photo = models.ImageField(upload_to=image_path, verbose_name=_("photo"))
-    bio = RichTextField(verbose_name=_("biography"))
+    photo = ImageField(upload_to=image_path, verbose_name=_('photo'))
+    bio = RichTextField(verbose_name=_('biography'))
+
+    def __unicode__(self):
+        return '{} {} {}'.format(self.last_name, self.first_name, self.middle_name)
+
+
+class Aspirant(models.Model):
+    last_name = models.CharField(_('last name'), max_length=30)
+    first_name = models.CharField(_('first name'), max_length=30)
+    middle_name = models.CharField(_('middle name'), max_length=30)
+    position_title = models.ForeignKey(Position, verbose_name=_('position'), related_name='a_position', blank=True, null=True)
+    science_title = models.ForeignKey(Science, verbose_name=_('science'), related_name='a_science')
+    contact = models.CharField(max_length=64, verbose_name=_('contacts'))
+    photo = ImageField(upload_to=image_path, verbose_name=_('photo'))
+    bio = RichTextField(verbose_name=_('biography'))
+
+    def __unicode__(self):
+        return '{} {} {}'.format(self.last_name, self.first_name, self.middle_name)
+
+
+class Technician(models.Model):
+    last_name = models.CharField(_('last name'), max_length=30)
+    first_name = models.CharField(_('first name'), max_length=30)
+    middle_name = models.CharField(_('middle name'), max_length=30)
+    position_title = models.ForeignKey(Position, verbose_name=_('position'), related_name='t_position', blank=True, null=True)
+    science_title = models.ForeignKey(Science, verbose_name=_('science'), related_name='t_science', blank=True, null=True)
+    contact = models.CharField(max_length=64, verbose_name=_('contacts'))
+    photo = ImageField(upload_to=image_path, verbose_name=_('photo'))
+    bio = RichTextField(verbose_name=_('biography'))
 
     def __unicode__(self):
         return '{} {} {}'.format(self.last_name, self.first_name, self.middle_name)
--- apps/cathedra/views.py
+++ apps/cathedra/views.py
@@ -1,13 +1,20 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import TemplateView
 
 from skd_tools.mixins import ActiveTabMixin
 
-from cathedra.models import Lector
+from cathedra.models import Lector, Aspirant, Technician
 
 
-class LectorsListView(ActiveTabMixin, ListView):
-    model = Lector
+class LectorsListView(ActiveTabMixin, TemplateView):
+    template_name = 'cathedra/lector_list.html'
     active_tab = 'academics'
+
+    def get_context_data(self, **kwargs):
+        context = super(LectorsListView, self).get_context_data(**kwargs)
+        context['lectors'] = Lector.objects.all()
+        context['aspirants'] = Aspirant.objects.all()
+        context['technicians'] = Technician.objects.all()
+        return context
 
 
 class HomeView(ActiveTabMixin, TemplateView):
